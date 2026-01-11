05ed415109b19b4ebe3ff3bbdbe235c11d55707e
Mirza-Muhammad-Jawad-Ali-Kazmi <mirzajawad804@gmail.com>
2026-01-11 12:21:40 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+    let h1 = document.querySelector("h1");
+window.addEventListener("keydown", function (dets) {
+    h1.textContent = dets.key;
+    if(dets.key === " ") {
+    h1.textContent = "space";
+    }
+        });
